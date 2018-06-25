de9bb518aa15777bfb375c845a57abbb079afff9
Cj Jordan <cj.jordan.tech@gmail.com>
2018-06-25 08:15:33 -0400

    adds post route test

--- test/test.js
+++ test/test.js
@@ -6,19 +6,43 @@ chai.should();
 
 chai.use(chaiHttp);
 
-describe("/GET /api/examples", function() {
+// describe("GET /api/examples", function() {
+//   after(function() {
+//     process.exit(0);
+//   });
+
+//   it("it should GET all the examples", function(done) {
+//     chai
+//       .request(server)
+//       .get("/api/examples")
+//       .end(function(err, res) {
+//         res.should.have.status(200);
+//         res.body.should.be.a("array");
+//         res.body.length.should.be.eql(0);
+//         done();
+//       });
+//   });
+// });
+
+describe("POST /api/examples", function() {
   after(function() {
     process.exit(0);
   });
 
-  it("it should GET all the examples", function(done) {
+  it("it should POST an example", function(done) {
+    var ex = {
+      text: "test",
+      description: "test entry"
+    };
     chai
       .request(server)
-      .get("/api/examples")
+      .post("/api/examples")
+      .send(ex)
       .end(function(err, res) {
         res.should.have.status(200);
-        res.body.should.be.a("array");
-        res.body.length.should.be.eql(0);
+        res.body.should.be.a("object");
+        res.body.should.have.property("id");
+        res.body.should.have.property("text").eql("test");
         done();
       });
   });
